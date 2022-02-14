f8adbdbed452633c5f80a4419636d6b6d2a14ace
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-02-14 15:23:11 +0100

    init

---
+++ src/test/java/com/example/newyear/NewYearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewYearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
