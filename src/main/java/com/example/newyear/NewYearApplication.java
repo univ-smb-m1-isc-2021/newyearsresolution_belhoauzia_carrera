b769a89d894ee0516af02e70a577990406e8be46
vastu <vatsuna74@gmail.com>
2022-03-14 16:38:08 +0100

    c toi le random

--- src/main/java/com/example/newyear/NewYearApplication.java
+++ src/main/java/com/example/newyear/NewYearApplication.java
@@ -1,17 +1,10 @@
 package com.example.newyear;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
 public class NewYearApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(NewYearApplication.class, args);
-    }
 
     @GetMapping("/hello")
     public String renderMainPage() {
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App.js";
-
 ReactDOM.render(
     <h1>Hello World</h1>,
     document.getElementById("reactEntryPoint")
