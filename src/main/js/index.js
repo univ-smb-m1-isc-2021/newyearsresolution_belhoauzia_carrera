296a8df0164d49a4d0facc83266441f8143b3aa2
vastu <vatsuna74@gmail.com>
2022-03-14 16:15:49 +0100

    target

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -4,6 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 
 ReactDOM.render(
-    <h1>Bonjour, monde !</h1>,
+    <h1>Hello World</h1>,
     document.getElementById("reactEntryPoint")
 );
