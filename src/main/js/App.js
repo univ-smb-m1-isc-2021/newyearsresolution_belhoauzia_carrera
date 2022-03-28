c5baa831d33c72405f461bdfd059245068d04aa1
vastu <vatsuna74@gmail.com>
2022-03-28 16:51:08 +0200

    logo

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -33,14 +33,14 @@ class App extends React.Component {
         return (
             <div>
                 <div class="mx-2 navbar">
-                    <figure className="image is-48x48 m-2">
+                    <figure className="image is-64x64 m-2">
                         <img src="/assets/logo.png" alt="logo"/>
                     </figure>
-                    <button class="button m-2 is-success" onClick={() => this.showComponent('Home')}>Home</button>
-                    <button class="button m-2 is-success" onClick={() => this.showComponent('Login')}>Login</button>
-                    <button class="button m-2 is-success" onClick={() => this.showComponent('Inscription')}>Inscription</button>
-                    <button className="button m-2 is-success" onClick={() => this.showComponent('MyResolution')}>MyResolution</button>
-                    <button className="button m-2 is-success" onClick={() => document.getElementById("popup-resolution").classList.add('is-active')}>AddResolution</button>
+                    <button class="button m-4 is-success" onClick={() => this.showComponent('Home')}>Home</button>
+                    <button class="button m-4 is-success" onClick={() => this.showComponent('Login')}>Login</button>
+                    <button class="button m-4 is-success" onClick={() => this.showComponent('Inscription')}>Inscription</button>
+                    <button className="button m-4 is-success" onClick={() => this.showComponent('MyResolution')}>MyResolution</button>
+                    <button className="button m-4 is-success" onClick={() => document.getElementById("popup-resolution").classList.add('is-active')}>AddResolution</button>
                 </div>
                 <div id="content m-6">
                     {components[this.state.displayedTable]}
