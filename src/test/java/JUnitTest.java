904ea4b981f40a6997c8fdd5a06ac325739d00a6
vastu <vatsuna74@gmail.com>
2022-04-09 17:47:26 +0200

    junit set up

---
+++ src/test/java/JUnitTest.java
@@ -0,0 +1,18 @@
+
+
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+public
+class JUnitTest extends TestCase{
+
+
+    @Test
+    public void test() {
+        System.out.println("simpleTest");
+        assertTrue(true);
+    }
+
+}
