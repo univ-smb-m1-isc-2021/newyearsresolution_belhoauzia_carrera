8a29da8355b9ed5cf06fd1ad4d7543b182b0ec4b
vastu <vatsuna74@gmail.com>
2022-04-09 18:33:50 +0200

    few more tests

---
+++ src/test/java/ResolutionServiceTest.java
@@ -0,0 +1,15 @@
+
+import com.example.newyear.service.ResolutionService;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public
+class ResolutionServiceTest extends TestCase{
+
+
+    @Test
+    public void test(){
+        ResolutionService rs = new ResolutionService(null,null,null);
+    }
+
+}
